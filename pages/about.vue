6dda36373f1ae6801c59e91cf5d09af182dc8cea
wen-3 <sd63324@gmail.com>
2024-11-26 16:42:08 +0800

    useSeoMeta
    了解 useSeoMeta & useServerSeoMeta

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,28 @@
-<script setup></script>
+<script setup>
+const text = ref("");
+
+useSeoMeta({
+  title: () => `${text.value} 關於我們 About`,
+  description: "About Vue 是屬於前端框架，主要著重在瀏覽器渲染，而有些...",
+  ogDescription: "About Vue 是屬於前端框架，主要著重在瀏覽器渲染，而有些...",
+  ogTitle: "About Nuxt3 直播",
+  ogImage: `/logo.png`,
+  twitterTitle: "About Nuxt3 直播",
+  twitterDescription: "About Vue 是屬於前端框架，主要著重在瀏覽器渲染，而有些...",
+  twitterCard: "summary_large_image",
+  twitterImage: `/logo.png`,
+});
+</script>
 
 <template>
   <div>
     <h2>關於我們</h2>
+    <input v-model="text" type="text" />
 
-    <NuxtLink to="/">回到首頁</NuxtLink>
-    <NuxtLink to="/address">聯絡地址</NuxtLink>
+    <div>
+      <NuxtLink to="/">回到首頁</NuxtLink>
+      <NuxtLink to="/address">聯絡地址</NuxtLink>
+    </div>
 
     <br>
 
