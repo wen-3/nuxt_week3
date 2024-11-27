86833c3ddaeaf5098d38f154bcb4f73094e06b26
wen-3 <sd63324@gmail.com>
2024-11-28 01:23:42 +0800

    middleware
    全域

--- middleware/check.global.js
+++ middleware/check.global.js
@@ -1,3 +1,14 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    console.log("我每一頁都可以攔截的到=>", to);
-});
+    // 取得 cookie
+    const auth = useCookie("auth");
+    // 需要驗證的頁面
+    const isNeedAuth = ["/about"];
+  
+    // 如果需要驗證的頁面，且 cookie 不存在，則導航到 /address
+    if (isNeedAuth.includes(to.path)) {
+      // 如果 cookie 不存在，則導航到 /404
+      if (!auth.value) {
+        return navigateTo("/404");
+      }
+    }
+  });  
